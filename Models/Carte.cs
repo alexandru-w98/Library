4c6fc93f1ba11944d559b6e3e96fd706f421f434
Alexandru Stoica <stoica.alex.w98@gmail.com>
2021-04-29 14:42:14 +0300

    Start

---
+++ Models/Carte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models
+{
+    public class Carte
+    {
+        public string ISBN { get; set; }
+        public string Nume { get; set; }
+        public double PretInchiriere { get; set; }
+    }
+}
---
+++ Repositories/Biblioteca.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Repositories
+{
+    public class LibraryRepository
+    {
+    }
+}
